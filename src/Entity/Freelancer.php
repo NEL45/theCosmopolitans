2a7b116d52fc0d0e78a64a6640d3730f3b531f27
Thuydieutran <thuy.dieutran86@gmail.com>
2021-06-29 14:48:32 +0200

    COmment

--- src/Entity/Freelancer.php
+++ src/Entity/Freelancer.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\FreelancerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -36,6 +38,16 @@ class Freelancer
      * @ORM\Column(type="string", length=255)
      */
     private string $password;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="freelance", orphanRemoval=true)
+     */
+    private $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -86,6 +98,36 @@ class Freelancer
     public function setPassword(string $password): self
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setFreelance($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getFreelance() === $this) {
+                $comment->setFreelance(null);
+            }
+        }
 
         return $this;
     }
