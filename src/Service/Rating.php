02d98d8e20561abf65c8ff53d39408d0012bab6c
Gersey Stelmach <gerseystelmach@gmail.com>
2021-06-29 23:27:45 +0200

    Service Avarage

--- src/Service/Rating.php
+++ src/Service/Rating.php
@@ -3,16 +3,17 @@
 namespace App\Service;
 
 use App\Entity\Client;
+use App\Repository\ClientRepository;
+use App\Repository\CommentRepository;
 
-class Rating {
+class Rating
+{
 
-
-    public function calculateAvarage(int $rating1, int $rating2, int $rating3, int $rating4): float 
+    public function calculateAvarage(int $rating1, int $rating2, int $rating3, int $rating4): float
     {
+
         $avarage = ($rating1 + $rating2 + $rating3 + $rating4) / 4;
 
         return $avarage;
     }
-
-
 }
